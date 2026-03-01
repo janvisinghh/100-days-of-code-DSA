c23a74d4758d38bfbde8ae02bf6d9a6db2bc4038
Janvi Singh <singhjanvi.1806@gmail.com>
2026-03-01 09:55:06 +0530

    Add files via upload

---
+++ day_29.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+struct Node* rotateRight(struct Node* head, int k) {
+    if (head == NULL || head->next == NULL || k == 0)
+        return head;
+
+    struct Node* temp = head;
+    int length = 1;
+
+    while (temp->next != NULL) {
+        temp = temp->next;
+        length++;
+    }
+
+    k = k % length;
+    if (k == 0)
+        return head;
+
+    temp->next = head;
+
+    int steps = length - k;
+    temp = head;
+
+    for (int i = 1; i < steps; i++) {
+        temp = temp->next;
+    }
+
+    head = temp->next;
+    temp->next = NULL;
+
+    return head;
+}
+
+void printList(struct Node* head) {
+    while (head != NULL) {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+}
+
+int main() {
+    int n, k, value;
+    struct Node *head = NULL, *tail = NULL;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+        struct Node* newNode = createNode(value);
+
+        if (head == NULL) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    scanf("%d", &k);
+
+    head = rotateRight(head, k);
+
+    printList(head);
+
+    return 0;
+}
