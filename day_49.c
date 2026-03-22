7d388f0f0ed8e67c24f453732ef2650716d79c95
Janvi Singh <singhjanvi.1806@gmail.com>
2026-03-22 21:56:44 +0530

    Add files via upload

---
+++ day_49.c
@@ -0,0 +1,5 @@
+int countLeaves(struct TreeNode* root) {
+    if (root == NULL) return 0;
+    if (root->left == NULL && root->right == NULL) return 1;
+    return countLeaves(root->left) + countLeaves(root->right);
+}
