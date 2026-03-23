44ca6dbf9ec386fcbc9261d4538b96a9d77150ff
Janvi Singh <singhjanvi.1806@gmail.com>
2026-03-23 09:56:08 +0530

    Add files via upload

--- day_51.c
+++ day_51.c
@@ -1,9 +1,59 @@
-struct TreeNode* searchBST(struct TreeNode* root, int val) {
-    if (root == NULL || root->val == val)
-        return root;
-    
-    if (val < root->val)
-        return searchBST(root->left, val);
-    
-    return searchBST(root->right, val);
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* newNode(int val) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+struct Node* insert(struct Node* root, int val) {
+    if (root == NULL) return newNode(val);
+
+    if (val < root->data)
+        root->left = insert(root->left, val);
+    else
+        root->right = insert(root->right, val);
+
+    return root;
+}
+
+struct Node* findLCA(struct Node* root, int n1, int n2) {
+    while (root != NULL) {
+        if (n1 < root->data && n2 < root->data)
+            root = root->left;
+        else if (n1 > root->data && n2 > root->data)
+            root = root->right;
+        else
+            return root;
+    }
+    return NULL;
+}
+
+int main() {
+    int n, val, n1, n2;
+    struct Node* root = NULL;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &val);
+        root = insert(root, val);
+    }
+
+    scanf("%d %d", &n1, &n2);
+
+    struct Node* lca = findLCA(root, n1, n2);
+
+    if (lca != NULL)
+        printf("%d\n", lca->data);
+
+    return 0;
 }
