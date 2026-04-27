c08dc1292be05e3f5e5cd5666eb4f39255c71259
Janvi Singh <singhjanvi.1806@gmail.com>
2026-04-27 17:50:00 +0530

    Add files via upload

---
+++ day_86.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int low = 0, high = n, ans = 0;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if ((long long)mid * mid <= n) {
+            ans = mid;
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    printf("%d", ans);
+    return 0;
+}
